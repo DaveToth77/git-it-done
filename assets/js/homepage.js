10942e7ea80b9bd8ba397d7408262e7823635faf
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-08-23 15:26:28 -0400

    added getUserRepos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
+let getUserRepos = function(user) {
+    //format the github api url
+    let apiUrl = "https://api.github.com/users/" + user + '/repos';
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+}
+
